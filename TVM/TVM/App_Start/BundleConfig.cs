045beae8bdc11f0a5d3fa25ce735135b326dc1a4
Chase Marcum <chase.m.marcum@gmail.com>
2014-05-29 11:19:10 -0700

    Updated UI

--- TVM/TVM/App_Start/BundleConfig.cs
+++ TVM/TVM/App_Start/BundleConfig.cs
@@ -24,8 +24,9 @@ namespace TVM
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap-slate.css",
+                      "~/Content/site.css",
+                      "~/Content/simple-sidebar.css"));
         }
     }
 }
