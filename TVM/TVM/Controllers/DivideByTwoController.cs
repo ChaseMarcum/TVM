3d4664be13829c9aa66bbf21363a582eaf846694
Chase M. Marcum <chase.m.marcum@gmail.com>
2014-05-28 20:28:25 -0700

    initial upload

---
+++ TVM/TVM/Controllers/DivideByTwoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TVM.Models;
+
+namespace TVM.Controllers
+{
+    public class DivideByTwoController : Controller
+    {
+        //
+        // GET: /DivideByTwo/
+        public ActionResult Index()
+        {
+            DivideByTwo function = new DivideByTwo();
+            return View(function);
+        }
+	}
+}
