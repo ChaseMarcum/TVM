cb8bb756396fdd874bc5915cf941c4197a0d18a9
johnprice2013 <john.price@oit.edu>
2014-05-30 17:19:01 -0700

    added timevaluemoney not equations

---
+++ TVM/TVM/Controllers/TimeValueMoneyController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TVM.Controllers
+{
+    public class TimeValueMoneyController : Controller
+    {
+        //
+        // GET: /TimeValueMoney/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /TimeValueMoney/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /TimeValueMoney/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /TimeValueMoney/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /TimeValueMoney/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /TimeValueMoney/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /TimeValueMoney/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /TimeValueMoney/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ TVM/TVM/Models/TimeValueMoney.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TVM.Models
+{
+    public class TimeValueMoney
+    {
+        public TimeValueMoney()
+        { 
+        
+        }
+
+
+        public string name = "Time Value Money";
+        public string description = "The description for the Time Value Money equation.";
+        public string firstVariableName = "NPER";
+        public string secondVariableName = "Rate";
+        public string thirdVariableName = "Present Value";
+        public string fourthVariableName = "Payment";
+        public string fifthVariableName = "Future Value";
+        public int chapter = 4;
+        public double firstVariable { get; set; }
+        public double secondVariable { get; set; }
+        public double thirdVariable { get; set; }
+        public double fourthVariable { get; set; }
+        public double fifthVariable { get; set; }
+    }
+}
