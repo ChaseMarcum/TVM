0869cf08f037fb2914d58e0f31a3b39a26aafcda
johnprice2013 <john.price@oit.edu>
2014-06-01 18:57:03 -0700

    added another equation and made tiny edity to layout oh, and added chapters to everything

--- TVM/TVM/Models/ThreeVarEquation.cs
+++ TVM/TVM/Models/ThreeVarEquation.cs
@@ -12,7 +12,8 @@ namespace TVM.Models
 
         }
 
-        public string[] equationName = { "Net Capital Spending",
+        public string[] equationName = { "Cash Flow from Assets",
+                                         "Net Capital Spending",
                                          "Operating Cash Flow",
                                          "Quick Ratio",
                                          "Return on Equity One",
@@ -20,31 +21,36 @@ namespace TVM.Models
                                       };
         public int[] chapter = { 2,
                                  2,
+                                 2,
                                  3,
                                  3,
                                  3};
 
         #region
-        public string[] description = {"This is the description for the Net Capital Spending Equation",
+        public string[] description = {"This is the description for the Cash Flow from Assets Equation",
+                                       "This is the description for the Net Capital Spending Equation",
                                        "This is the description for the Operating Cash Flow Equation",
                                        "This is the description for the Quick Ratio Equation",
                                        "This is the description for the Return on Equity Equation",
                                        "This is the description for the Return on Equity Equation"};
         #endregion
         #region
-        public string[] firstVarName = {"NFA(end)",
+        public string[] firstVarName = {"Operating Cash Flow",
+                                        "NFA(end)",
                                         "EBIT",
                                         "Current Assets",
                                         "Profit Margin",
                                         "Net Income"};
 
-        public string[] secondVarName = {"NFA(beg)",
+        public string[] secondVarName = {"Change in Net Working Capital",
+                                         "NFA(beg)",
                                          "Depreciation",
                                          "Inventory",
                                          "Total Asset Turnover",
                                          "(Total Assets"};
 
-        public string[] thirdVarName = {"Depreciation",
+        public string[] thirdVarName = {"Net Capital Spending",
+                                        "Depreciation",
                                         "Taxes",
                                         "Current Liabilities",
                                         "Equity Mulitiplier",
@@ -52,18 +58,21 @@ namespace TVM.Models
         #endregion
         #region
         public string[] firstEquationOperator = {"-",
+                                                 "-",
                                                  "+",
                                                  "-",
                                                  "*",
                                                  "/"};
 
-        public string[] secondEquationOperator = {"+",
+        public string[] secondEquationOperator = {"-",
+                                                  "+",
                                                   "-",
                                                   "/",
                                                   "*",
                                                   "-"};
         #endregion
         public string[] resultType = {"Dollar",
+                                      "Dollar",
                                       "Dollar",
                                       "Ratio",
                                       "Percent",
