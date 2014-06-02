165160efa09c285bf93eece52f445be2ecc0c195
johnprice2013 <john.price@oit.edu>
2014-06-01 18:31:34 -0700

    added new equation to three var equations woot

--- TVM/TVM/Models/ThreeVarEquation.cs
+++ TVM/TVM/Models/ThreeVarEquation.cs
@@ -15,10 +15,12 @@ namespace TVM.Models
         public string[] equationName = { "Net Capital Spending",
                                          "Operating Cash Flow",
                                          "Quick Ratio",
-                                         "Return on Equity One"
+                                         "Return on Equity One",
+                                         "Return on Equity Two"
                                       };
         public int[] chapter = { 2,
                                  2,
+                                 3,
                                  3,
                                  3};
 
@@ -27,38 +29,44 @@ namespace TVM.Models
                                        "This is the description for the Operating Cash Flow Equation",
                                        "This is the description for the Quick Ratio Equation",
                                        "This is the description for the Return on Equity Equation",
-                                       };
+                                       "This is the description for the Return on Equity Equation"};
         #endregion
         #region
         public string[] firstVarName = {"NFA(end)",
                                         "EBIT",
                                         "Current Assets",
-                                        "Profit Margin"};
+                                        "Profit Margin",
+                                        "Net Income"};
 
         public string[] secondVarName = {"NFA(beg)",
                                          "Depreciation",
                                          "Inventory",
-                                         "Total Asset Turnover"};
+                                         "Total Asset Turnover",
+                                         "(Total Assets"};
 
         public string[] thirdVarName = {"Depreciation",
                                         "Taxes",
                                         "Current Liabilities",
-                                        "Equity Mulitiplier"};
+                                        "Equity Mulitiplier",
+                                        "Total Debt)"};
         #endregion
         #region
         public string[] firstEquationOperator = {"-",
                                                  "+",
                                                  "-",
-                                                 "*"};
+                                                 "*",
+                                                 "/"};
 
         public string[] secondEquationOperator = {"+",
                                                   "-",
                                                   "/",
-                                                  "*"};
+                                                  "*",
+                                                  "-"};
         #endregion
         public string[] resultType = {"Dollar",
                                       "Dollar",
                                       "Ratio",
+                                      "Percent",
                                       "Percent"};
         public double firstVariable { get; set; }
         public double secondVariable { get; set; }
