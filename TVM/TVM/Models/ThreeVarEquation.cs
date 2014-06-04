0af055421403423b1ca297b6a8f946f3c8109993
Chase Marcum <chase.m.marcum@gmail.com>
2014-06-04 14:39:45 -0700

    Updated 3 Var

--- TVM/TVM/Models/ThreeVarEquation.cs
+++ TVM/TVM/Models/ThreeVarEquation.cs
@@ -29,10 +29,10 @@ namespace TVM.Models
         #region
         public string[] description = {"To calculate the Cash Flow from Assets you will need to take the total of the cash flow to creditors and the cash flow to stockholders, that is consisting of the operating cash flow, the capital spending, and the changes in net working capital.",
                                        "To calculate the Net Capital Spending you will need to take the Ending Net Fixed Asset and subtract it by the Beginning Net Fixed Assets and the Depreciation",
-                                       "This is the description for the Operating Cash Flow Equation",
-                                       "This is the description for the Quick Ratio Equation",
-                                       "This is the description for the Return on Equity Equation",
-                                       "This is the description for the Return on Equity Equation"};
+                                       "Operating Cash Flow is the cash generated from normal activity of the business.  To calculate take the Earnings Before Interest & Tax add the Depreciation and subtract the Taxes.",
+                                       "The Quick Ratio is the measure of the short-term liquidity, after removing the effects of inventory.  To calculate you take the Current Assets subtracted by the Inventory and divide the result by the Current Liabilities.",
+                                       "The Return on Equity or ROE is the ratio of the bottom-line profit per dollar of equity.  This is the first example of two possible ways to calculate the ROE. In this method to calculate the ROE take the Profit Margin and multiply it by the Total Asset Turnover and then multiply the result by the Equity Multiplier.",
+                                       "The Return on Equity or ROE is the ratio of the bottom-line profit per dollar of equity.  This is the second example of two possible ways to calculate the ROE. In this method to calculate the ROE take the Net Income divided by the result of the Total Assets subtracted by the Total Debt."};
         #endregion
         #region
         public string[] firstVarName = {"Operating Cash Flow",
