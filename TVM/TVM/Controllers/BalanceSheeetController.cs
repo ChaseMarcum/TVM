ce6335397df64350e585d5666bd69a8fc49c7de9
Chase M. Marcum <chase.m.marcum@gmail.com>
2014-06-04 20:06:39 -0700

    Update

---
+++ TVM/TVM/Controllers/BalanceSheeetController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TVM.Controllers
+{
+    public class BalanceSheetController : Controller
+    {
+        // GET: BalabceSheet
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ TVM/TVM/Controllers/IncomeStatementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TVM.Controllers
+{
+    public class IncomeStatementController : Controller
+    {
+        // GET: IncomeStatement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
